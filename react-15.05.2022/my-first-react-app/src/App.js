1ad287fadbad078418fcbda0740b3697e265a765
hibara89 <hiba.rashed@gmail.com>
2022-05-15 13:34:46 +0300

    introduction to react

---
+++ react-15.05.2022/my-first-react-app/src/App.js
@@ -0,0 +1,47 @@
+import React from "react";
+
+React.createElement("a", {
+  className: "",
+  id: "",
+});
+
+class App extends React.Component {
+  constructor() {
+    super();
+  }
+  render() {
+    return (
+      // <div className="" data-isopen="">
+      //   Hello world from App Component
+      // </div>
+      <a href=" ">simple text</a>
+    );
+  }
+}
+export default App;
+
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
