5d7d185d197d135f41b2e37c423e37c37136d4da
hibara89 <hiba.rashed@gmail.com>
2022-07-06 09:32:41 +0300

    yes

---
+++ reactCourse-2022/my-app/src/book.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Bookone = () => {
+  return <div> Book</div>;
+};
--- reactCourse-2022/my-app/src/index.js
+++ reactCourse-2022/my-app/src/index.js
@@ -1,40 +1,70 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Bookone } from "./book";
 import "./index.css";
+
+const firstBook = {
+  img: "https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/mid/9780/2419/9780241988725.jpg",
+  title: "title book",
+  author: "hiba",
+};
+const secondBook = {
+  img: "https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/mid/9780/2419/9780241988725.jpg",
+  title: "title book2",
+  author: "hiba",
+};
 
 function BookList() {
   return (
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      <Bookone />
+      <Book
+        img={firstBook.img}
+        title={firstBook.title}
+        author={firstBook.author}
+      >
+        <p>this book is very special</p>
+      </Book>
+      <Book
+        img={secondBook.img}
+        title={secondBook.title}
+        author={secondBook.author}
+      />
     </section>
   );
 }
 
-const Book = () => {
+//object destructuring
+const Book = ({ img, title, author, children }) => {
+  // console.log(props);
+  // const { img, title, author } = props;
   return (
     <article className="book">
-      <Image />
-      <Title />
-      <Author />
+      <img src={img} alt="" />
+      <h1>{title}</h1>
+      <h4 style={{ color: "#617d98", fontSize: "0.75rem" }}>{author}</h4>
+      <p>{children}</p>
     </article>
   );
 };
 
-const Image = () => (
-  <img
-    src="https://d1w7fb2mkkr3kw.cloudfront.net/assets/images/book/mid/9780/2419/9780241988725.jpg"
-    alt=""
-  />
-);
-
-const Title = () => <h1>the book title</h1>;
-const Author = () => (
-  <h4 style={{ color: "#617d98", fontSize: "0.75rem" }}>the author name</h4>
-);
+const names = ["john", "peter", "susan"];
+const newNames = names.map;
+<img src={img} alt="" />;
+const names = ["john", "peter", "susan"];
+const newNames = names.map;
+<img src={img} alt="" />;
+const names = ["john", "peter", "susan"];
+const newNames = names.map;
+<img src={img} alt="" />;
+const names = ["john", "peter", "susan"];
+const newNames = names.map;
+<img src={img} alt="" />;
+const names = ["john", "peter", "susan"];
+const newNames = names.map;
+<img src={img} alt="" />;
+const names = ["john", "peter", "susan"];
+const newNames = names.map;
+<img src={img} alt="" />;
 
 ReactDOM.render(<BookList></BookList>, document.getElementById("root"));
