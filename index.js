e07f290a4f1180da4ae3c3885f8110a36f9a4728
hibara89 <102147745+hibara89@users.noreply.github.com>
2022-07-08 16:00:05 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,45 +1,11 @@
-// function isValidPassword(password, username) {
-//   if (password.length < 8) {
-//     return false;
-//   }
-//   if (password.indexOf(" ") !== -1) {
-//     return false;
-//   }
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
 
-//   if (password.indexOf(username) !== -1) {
-//     return false;
-//   }
-// }
-
-// implicit return
-
-// const square = n => {
-
-//   return n*n;
-// }
-/*
-const square = (n) => n * n;
-
-console.log(square(10));
-
-const nums = [1, 2, 3, 4, 5, 6, 7, 8];
-
-//to map into array even or odd
-
-const parityList = nums.map((n) => {
-  if (n % 2 === 0) return "even";
-  return "odd";
-});
-
-//in one line
-
-const parityList = nums.map((n) => (n % 2 === 0 ? "even" : "odd"));
-*/
-// function welcome() {
-//   let welcome = "Welcome to Appleseeds Bootcamp!";
-//   return welcome;
-// }
-
-// const power = (a) => Math.pow(a, 2);
-
-// console.log(power(4));
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
